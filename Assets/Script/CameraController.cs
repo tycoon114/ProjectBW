d2c84fb89aca136c0ed9bddcb1dd43d8191f10ac
civalwar114 <sly114@naver.com>
2026-03-24 11:53:08 +0900

    카메라 컨트롤러
    기초적인 셋팅

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private Vector3 lookPosition; // 보는 위치
 
-    // Update is called once per frame
+    private float pitch = 0f;                                       //위아래 회전
+    private float yaw = 0f;                                         //좌우 회전
+    private float defaultFov = 40f;                                 //기본 시야각
+    private float zoomFov = 20f;                                    //줌 시야각
+    public float zoomSpeed = 5.0f;                                  //확대축소가 되는 속도
+    public float mouseSensitivity = 60.0f;                          //감도
+
+
+
+    // Update is called once per frame 
     void Update()
     {
         
     }
+
+    private void LateUpdate()
+    {
+        
+    }
+
 }
