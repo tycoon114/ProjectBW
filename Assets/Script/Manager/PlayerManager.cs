f27becc48e7643ffd3573c9b3bc8ce8ec46a8a0f
civalwar114 <sly114@naver.com>
2026-04-16 15:21:35 +0900

    플레이어 매니저 주석
    의사 코드 작성중

--- Assets/Script/Manager/PlayerManager.cs
+++ Assets/Script/Manager/PlayerManager.cs
@@ -2,7 +2,17 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   
+
+    /*
+        작동 방식 설계
+     체력, 공격력 등의 정보는 따로 받아온다.
+    캐릭터가 생성 될때 받아올것 (현재 테스트 씬에서는 생략), 캐릭터 명을 대조하여 해당하는 정보를 받아옴
+    컬럼 : 캐릭터명, 공격력, 체력
+    피격을 받으면 이 스크립트를 호출?
+     
+     */
+
     void Start()
     {
         
